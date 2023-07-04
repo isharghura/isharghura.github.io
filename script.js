a33d51c66d39add972c97cb9106235483aaef086
isharghura <ishar.ghuras@gmail.com>
2023-07-04 16:18:24 -0400

    saved darkmode preference even after reloading page

--- script.js
+++ script.js
@@ -67,52 +67,75 @@ let tabLinks = document.querySelectorAll('.tab-links');
 let projImg = document.querySelectorAll('.work img:not(:last-child)')
 let menuButton = document.querySelector('.fas.fa-bars')
 let contactForm = document.querySelectorAll('.container form input, textarea, button');
+let count = 0;
 
 function darkmode() {
-    var body = document.body;
-
-    if (darkmodebtn.style.color === "black") {
-        body.style.backgroundColor = "black";
-        body.style.color = "white";
-        header.style.background = 'black';
-        nameText.style.color = "white";
-        darkmodebtn.style.color = 'white';
-        tabLinks.forEach(function (link) {
-            link.style.color = 'white';
-        })
-        projImg.forEach(function (img) {
-            img.style.border = '10px solid #DCDCDC';
-        })
-        menuButton.style.color = 'white';
-        contactForm.forEach(function (element) {
-            element.style.color = 'white';
-            element.style.border = '1px solid white'
-        })
-        darkmodebtn.style.border = 'transparent';
-        mybutton.style.border = 'transparent';
-        mybutton.style.color = 'white';
-
+    count++;
+    if (count % 2 == 1) {
+        enableDarkMode();
         localStorage.setItem('darkModeEnabled', 'true');
-    } else {
-        body.style.backgroundColor = "white";
-        body.style.color = "black";
-        header.style.background = 'white';
-        nameText.style.color = "black";
-        darkmodebtn.style.color = 'black';
-        tabLinks.forEach(function (link) {
-            link.style.color = 'black';
-        })
-        projImg.forEach(function (img) {
-            img.style.border = '10px solid black';
-        })
-        menuButton.style.color = 'black';
-        contactForm.forEach(function (element) {
-            element.style.color = 'black';
-            element.style.border = '1px solid black'
-        })
-        darkmodebtn.style.border = 'transparent';
-        mybutton.style.border = 'transparent';
-        mybutton.style.color = 'white';
+    }
+    else {
+        disableDarkMode();
         localStorage.setItem('darkModeEnabled', 'false');
     }
 }
+
+function enableDarkMode() {
+    var body = document.body;
+    body.style.backgroundColor = "black";
+    body.style.color = "white";
+    header.style.background = 'black';
+    nameText.style.color = "white";
+    darkmodebtn.style.color = 'white';
+    tabLinks.forEach(function (link) {
+        link.style.color = 'white';
+    })
+    projImg.forEach(function (img) {
+        img.style.border = '10px solid #DCDCDC';
+    })
+    menuButton.style.color = 'white';
+    contactForm.forEach(function (element) {
+        element.style.color = 'white';
+        element.style.border = '1px solid white'
+    })
+    darkmodebtn.style.border = 'transparent';
+    mybutton.style.border = 'transparent';
+    mybutton.style.color = 'white';
+
+    localStorage.setItem('darkModeEnabled', 'true');
+}
+
+function disableDarkMode() {
+    var body = document.body;
+    body.style.backgroundColor = "white";
+    body.style.color = "black";
+    header.style.background = 'white';
+    nameText.style.color = "black";
+    darkmodebtn.style.color = 'black';
+    tabLinks.forEach(function (link) {
+        link.style.color = 'black';
+    })
+    projImg.forEach(function (img) {
+        img.style.border = '10px solid black';
+    })
+    menuButton.style.color = 'black';
+    contactForm.forEach(function (element) {
+        element.style.color = 'black';
+        element.style.border = '1px solid black'
+    })
+    darkmodebtn.style.border = 'transparent';
+    mybutton.style.border = 'transparent';
+    mybutton.style.color = 'white';
+    localStorage.setItem('darkModeEnabled', 'false');
+}
+
+window.addEventListener('load', function(){
+    var darkModePreference=this.localStorage.getItem('darkModeEnabled');
+    if(darkModePreference==='true'){
+        enableDarkMode();
+    }
+    else{
+        disableDarkMode();
+    }
+})
